159db38c0aa297af42f07eab479decd7565f3bab
M3RIGI <jamesmerigi@gmail.com>
2023-06-04 22:42:00 +0300

    Updated

---
+++ EmployeeAppVSCode/src/EmployeeAppVSCode.EmployeeComponent/Employee.cs
@@ -0,0 +1,13 @@
+namespace EmployeeAppVSCode.EmployeeComponent
+
+{ 
+    public class Employee
+    { public int ID { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public char Gender { get; set; }
+
+    }
+
+} 
+    
---
+++ EmployeeAppVSCode/src/EmployeeAppVSCode.EmployeeConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmployeeAppVSCode.EmployeeComponent;
+using System.Collections.Generic;
+
+namespace EmployeeAppVSCode.EmployeeConsoleApp
+{
+
+    class Program
+    {
+
+        static void Main (string[] args)
+        {
+            List<Employee> employees = new List<Employee> ();
+
+            var employee = new Employee{
+                ID = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Gender = 'm'
+                
+            }; 
+
+            employees.Add(employee);
+
+            employee = new Employee {
+                ID = 2,
+                FirstName = "Bob",
+                LastName = "Moe",
+                Gender = 'm'
+            };
+            employees.Add(employee);
+
+            employee = new Employee {
+                ID = 3,
+                FirstName = "Mary",
+                LastName = "Jane",
+                Gender = 'f'
+            };
+
+            employees.Add (employee);
+
+            foreach(var emp in employees)
+            {
+                Console.WriteLine($"{emp.ID.ToString().PadRight(6)}{emp.FirstName.PadRight(15)}{emp.LastName.PadRight(15)}{emp.Gender.ToString().PadRight(20)}");
+            }            
+            }
+
+                   }
+  }
+
